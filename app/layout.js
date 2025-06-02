d9ebeefb0e295fa89ee9509bd687bacbec08e2ed
dt313 <dt313.dev21@gmail.com>
2025-06-02 22:04:14 +0900

    chore: add image for seo , fix mobile notification dropdown css

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import images from '@/assets/images';
+
 import AuthFormWrap from '@/components/auth-form-wrap';
 import ImagePreviewWrap from '@/components/image-preview/ImagePreviewWrap';
 import StoreProvider from '@/components/store-provider';
@@ -11,6 +13,18 @@ export const metadata = {
     description: 'Chat application for retro design enthusiasts',
     icons: {
         icon: '/favicon.ico',
+    },
+
+    openGraph: {
+        title: 'Retro',
+        description: 'Chat application for retro design enthusiasts',
+        images: [images.largeLogo], // ảnh này đặt trong thư mục public
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: 'Retro',
+        description: 'Chat application for retro design enthusiasts',
+        images: [images.largeLogo], // nên dùng cùng ảnh với openGraph
     },
 };
 
