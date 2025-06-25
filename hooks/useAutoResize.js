76ce5d00ad0b6dd577ee68b2c263a6bbb2aa409c
dt313 <dt313.dev21@gmail.com>
2025-06-26 03:12:16 +0900

    chore: change conversation ui in mobile

--- hooks/useAutoResize.js
+++ hooks/useAutoResize.js
@@ -6,6 +6,10 @@ function useAutoResize(value) {
     const ref = useRef();
 
     useEffect(() => {
+        if (value === undefined || value === null || value === '') {
+            ref.current.style.height = window.getComputedStyle(ref.current).minHeight;
+            return;
+        }
         ref.current.style.height = 'auto';
         ref.current.style.height = ref.current.scrollHeight + 'px';
     }, [value]);
