d9805a9462f790db61b4f0acb728facb0a8d8004
dt313 <dt313.dev21@gmail.com>
2025-07-15 00:29:20 +0900

    chore: fix theme select

--- components/setting-box/conversation-theme-setting/ConversationThemeSetting.js
+++ components/setting-box/conversation-theme-setting/ConversationThemeSetting.js
@@ -3,6 +3,7 @@ import { useMemo, useState } from 'react';
 import classNames from 'classnames/bind';
 
 import { THEME_SETTING_SAMPLE_MESSAGE, getConversationTheme } from '@/config/ui-config';
+import { useTheme } from 'next-themes';
 
 import findConversationThemeByName from '@/helpers/find-conversation-theme-by-name';
 
@@ -11,41 +12,40 @@ import styles from './ConversationThemeSetting.module.scss';
 const cx = classNames.bind(styles);
 
 function ConversationThemeSetting({ onChangeTheme, value }) {
-    const [theme, setTheme] = useState(findConversationThemeByName(value) || CONVERSATION_THEME_LIST[0]);
+    const [themeValue, setThemeValue] = useState(findConversationThemeByName(value) || getConversationTheme()[0]);
 
-    const handleChangeTheme = useMemo(
-        (id) => {
-            const selectedTheme = getConversationTheme().find((item) => item.id === id);
-            setTheme(selectedTheme);
-            onChangeTheme(selectedTheme.name);
-        },
-        [theme],
-    );
+    const handleChangeTheme = (id) => {
+        const selectedTheme = getConversationTheme().find((item) => item.id === id);
+        setThemeValue(selectedTheme);
+        onChangeTheme(selectedTheme?.name);
+    };
+
     return (
         <div className={cx('wrapper')}>
             <div className={cx('list')}>
-                {getConversationTheme().map((item) => (
-                    <div
-                        className={cx('item', { active: item.id === theme.id })}
-                        key={item.id}
-                        onClick={() => handleChangeTheme(item.id)}
-                    >
-                        <div className={cx('item-image')}>
-                            {/* <Image className={cx('img')} src={images.fbIcon} />
-                             */}
-                            <span
-                                className={cx('color-box')}
-                                style={{
-                                    backgroundColor: item.boxColor,
-                                }}
-                            ></span>
+                {getConversationTheme().length > 0 &&
+                    getConversationTheme().map((item) => (
+                        <div
+                            className={cx('item', { active: item?.id === themeValue?.id })}
+                            key={item?.id}
+                            onClick={() => handleChangeTheme(item?.id)}
+                        >
+                            <div className={cx('item-image')}>
+                                {/* <Image className={cx('img')} src={images.fbIcon} />
+                                 */}
+                                <span
+                                    className={cx('color-box')}
+                                    style={{
+                                        backgroundColor: item?.boxColor,
+                                    }}
+                                ></span>
+                            </div>
+                            <div className={cx('item-name')}>{item?.title}</div>
                         </div>
-                        <div className={cx('item-name')}>{item.title}</div>
-                    </div>
-                ))}
+                    ))}
             </div>
             <div className={cx('preview')}>
-                <div className={cx('preview-content')} style={{ backgroundColor: theme.styles.backgroundColor }}>
+                <div className={cx('preview-content')} style={{ backgroundColor: themeValue?.styles?.backgroundColor }}>
                     {THEME_SETTING_SAMPLE_MESSAGE.map((message) => (
                         <div
                             key={message.id}
@@ -55,10 +55,10 @@ function ConversationThemeSetting({ onChangeTheme, value }) {
                                 className={cx('message-text')}
                                 style={{
                                     backgroundColor: message.isSender
-                                        ? theme.styles.senderBackgroundColor
-                                        : theme.styles.receiverBackgroundColor,
-                                    color: theme.styles.textColor,
-                                    boxShadow: `${theme.styles.messageBoxShadow}`,
+                                        ? themeValue?.styles?.senderBackgroundColor
+                                        : themeValue?.styles?.receiverBackgroundColor,
+                                    color: themeValue.styles.textColor,
+                                    boxShadow: `${themeValue?.styles?.messageBoxShadow}`,
                                 }}
                             >
                                 {message.content}
